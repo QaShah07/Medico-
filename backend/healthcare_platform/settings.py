6665a8bee6bfe95b8a50606073a79508680dd27d
Shah Mohammad <shahiitp07@gmail.com>
2025-08-05 22:39:17 +0530

    final deploy

--- backend/healthcare_platform/settings.py
+++ backend/healthcare_platform/settings.py
@@ -116,6 +116,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://medico-bay-delta.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True  # Only for development
